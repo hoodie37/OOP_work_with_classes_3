5f368f0c4734a698424b355249a346b047342416
Василий Яковлев <vasya_anime@mail.ru>
2020-09-21 17:36:51 +0500

    Добавьте файлы проекта.

---
+++ 4_lab/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace _4_lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBox
+            // 
+            this.pBox.Location = new System.Drawing.Point(0, 0);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(1157, 624);
+            this.pBox.TabIndex = 0;
+            this.pBox.TabStop = false;
+            this.pBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pBox_Paint);
+            this.pBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pBox_MouseClick);
+            this.pBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pBox_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(12, 53);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(136, 25);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Удалить Выделенные";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 623);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ 4_lab/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_lab
+{
+    public partial class Form1 : Form
+    {
+        MyList l = new MyList();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+        private void pBox_MouseClick(object sender, MouseEventArgs e)
+        {
+
+
+            int flag = 0;
+
+            for (l.First(); l.Eol(); l.Next())
+            {
+                if (l.GetObj().mouseInKrug(e.X, e.Y) == true)
+                {
+                    if (l.GetObj().GetColor() == 1)
+                        l.GetObj().SwitchColor(2);
+                    else l.GetObj().SwitchColor(1);
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag != 1)
+            {
+                l.add(new krug(e.X, e.Y, 1));
+            }
+            this.Refresh();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = e.X.ToString();
+            label2.Text = e.Y.ToString();
+        }
+
+        private void pBox_Paint(object sender, PaintEventArgs e)
+        {
+            for (l.First(); l.Eol(); l.Next())
+            {
+                l.GetObj().DrawKrug(l.GetObj(), e.Graphics);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            for (l.First(); l.Eol(); l.Next())
+            {
+                if (l.GetObj().GetColor() == 2)
+                {
+                    l.delete(l.GetObj());
+                }
+                if (l.nalichie() == true && l.GetNextNode() == null)
+                {
+
+                    if (l.GetHeadObj().GetColor() == 2)
+                    {
+                        l.delete(l.GetHeadObj());
+                    }
+                }
+
+            }
+            this.Refresh();
+        }
+    }
+
+    public class krug
+    {
+        public int diam = 0;
+        public int x = 0;
+        public int y = 0;
+        public int color = 0;
+
+        public krug(int dx, int dy, int color)
+        {
+            x = dx;
+            y = dy;
+            diam = 200;
+            this.color = color;
+        }
+
+        public void SwitchColor(int c)
+        {
+            color = c;
+        }
+
+        public int GetColor()
+        {
+            return color;
+        }
+
+        public bool mouseInKrug(int dx, int dy)
+        {
+            return (((dx - x) * (dx - x) + (dy - y) * (dy - y)) <= ((diam / 2) * (diam / 2)));
+        }
+
+        public void DrawKrug(krug k, Graphics g)
+        {
+            Pen pen;
+            if (k.color == 1) pen = new Pen(Brushes.Blue);
+            else pen = new Pen(Brushes.Red);
+            g.DrawEllipse(pen, k.x - diam / 2, k.y - diam / 2, diam, diam);
+        }
+    }
+
+    public class Node
+    {
+        public krug data;
+        public Node next;
+
+        public Node(krug value)
+        {
+            data = value;
+        }
+    }
+
+    public class MyList
+    {
+        public Node Head;
+        public Node Tail;
+        public int count;
+        public Node current;
+        public MyList()
+        {
+            Head = null;
+            Tail = null;
+            current = Head;
+            count = 0;
+        }
+
+        public MyList(krug value)
+        {
+            var node = new Node(value);
+            Head = node;
+            current = Head;
+            Tail = node;
+            count++;
+        }
+
+        public MyList(Node node)
+        {
+            Head = node;
+            Tail = node;
+            current = Head;
+            count++;
+        }
+
+
+
+        public krug GetObj()
+        {
+            return current.data;
+        }
+
+        public bool Eol()
+        {
+            if (current == null)
+                return false;
+
+            return true;
+        }
+
+        public void First()
+        {
+            current = Head;
+        }
+
+        public void Next()
+        {
+            if (current != null)
+                current = current.next;
+
+        }
+
+        public Node GetNode()
+        {
+            return current;
+        }
+
+        public Node GetNextNode()
+        {
+            return current.next;
+        }
+
+        public krug GetHeadObj()
+        {
+            return Head.data;
+        }
+
+        public void add(krug value)
+        {
+            var node = new Node(value);
+            if (Head == null)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.next = node;
+            }
+            Tail = node;
+            count++;
+        }
+
+
+
+
+        public void delete(krug value)
+        {
+
+            if (Head != null)
+            {
+                if (Head.data.Equals(value))
+                {
+                    if (current.next != Tail && current.next != null)
+                    {
+                        Head = Head.next;
+                        count--;
+
+                        current = Head;
+                        return;
+                    }
+                    else
+                    {
+                        if (current.next == Tail)
+                        {
+                            Head = Tail;
+                            current = Head;
+                            count--;
+                            return;
+                        }
+                        else
+                        {
+                            Head = null;
+                            Tail = null;
+                            current = null;
+                            return;
+                        }
+                    }
+                }
+
+                current = Head.next;
+                var pred = Head;
+
+                while (current != null)
+                {
+                    if (current.data.Equals(value))
+                    {
+                        if (current == Tail && pred == Head)
+                        {
+                            Tail = Head;
+                            current = Head;
+                            current.next = null;
+                            return;
+                        }
+                        if (current.next == null)
+                        {
+                            Tail = pred;
+                            count--;
+                            current = Tail;
+                            current.next = null;
+                            return;
+                        }
+                        else
+                        {
+                            pred.next = current.next;
+                            count--;
+
+                            current = pred;
+                            return;
+                        }
+                    }
+                    pred = current;
+                    current = current.next;
+                }
+            }
+            return;
+        }
+
+        public bool nalichie()
+        {
+
+            if (Head != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
